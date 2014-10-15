aac9fe10760d9b44adf80e41ee37257810ed21d4
YiniYin <yinnski@gmail.com>
2014-10-16 05:03:47 +1300

    modifies unit tests for signin service

--- carbone-iron/test/spec/servicesSpec.js
+++ carbone-iron/test/spec/servicesSpec.js
@@ -2,76 +2,66 @@
 
 /* jasmine specs for services go here */
 describe('SignInService', function() {
+  var signInService, $httpBackend;
 
   beforeEach(module('CarboneIron'));
 
-  describe('SignInCtrl', function(){
-    var scope, ctrl, $httpBackend;
+  beforeEach(inject(function(_signInService_ _$httpBackend_){
+    signInService = _signInService_;
+    $httpBackend = _$httpBackend_;
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
-      $httpBackend = _$httpBackend_;
+    // define a general http mock here.
+    $httpBackend.when('GET', '/api/auth')
+      .respond([{userId: 'userX'}, {'token': 'xyz'}, {'message': ''}]);
+  }));
 
-      $httpBackend.when('GET', '/api/auth').
-      respond([{userId: 'userX'}, {'token': 'xyz'}, {'message': ''}]);
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
 
-      scope = $rootScope.$new();
-      ctrl = $controller('SignInCtrl', {$scope: scope});
+  it('should get sign in successful', function() {
+    $httpBackend.expect('GET', 'https://api.carboneiron.com/signin')
+       .respond(200, "[{ success : 'true', id : 123 }]");
 
-    }));
-
-    afterEach(function() {
-      $httpBackend.verifyNoOutstandingExpectation();
-      $httpBackend.verifyNoOutstandingRequest();
-    });
-
-
-    it('should get sign in success',
-      inject(function(SignInService, $httpBackend) {
-
-      $httpBackend.expect('GET', 'https://api.CarboneIron.com/signin')
-        .respond(200, "[{ success : 'true', id : 123 }]");
-
-      LoginService.login('test@test.com', 'password')
-        .then(function(data) {
-          expect(data.success).toBeTruthy();
+    signInService.signin('test@test.com', 'password')
+       .then(function(data) {
+         expect(data.success).toBeTruthy();
       });
 
-      $httpBackend.flush();
-    });
-
-    it('should have a working SignInService service', inject(['SignInService',
-      function(SignInService){
-        expect(SignInService.isValidUsername).not.to.equal(null);
-
-
-        // test cases - testing for success
-        var validUsername = [
-          'test@test.com',
-          'test@test.co.uk',
-          'test734ltylytkliytkryety9ef@jb-fe.com'
-        ];
-
-        // test cases - testing for failure
-        var invalidUsername = [
-          'test@testcom',
-          'test@ test.co.uk',
-          'ghgf@fe.com.co.',
-          'tes@t@test.com',
-          ''
-        ];
-
-        // you can loop through arrays of test cases like this
-        for (var i in validUsername) {
-          var valid = SignInService.isValidUsername(validUsername[i]);
-          expect(valid).toBeTruthy();
-        }
-        for (var i in invalidUsername) {
-          var valid = SignInService.isValidUsername(invalidUsername[i]);
-          expect(valid).toBeFalsy();
-        }
-      }])
-    );
-
+    $httpBackend.flush();
   });
+
+  it('should have a working SignInService service',
+     inject(['SignInService', function(SignInService){
+       expect(SignInService.isValidUsername).not.to.equal(null);
+
+       // test cases - testing for success
+       var validUsername = [
+         'test@test.com',
+         'test@test.co.uk',
+         'test734ltylytkliytkryety9ef@jb-fe.com'
+       ];
+
+       // test cases - testing for failure
+       var invalidUsername = [
+         'test@testcom',
+         'test@ test.co.uk',
+         'ghgf@fe.com.co.',
+         'tes@t@test.com',
+         ''
+       ];
+
+       // you can loop through arrays of test cases like this
+       for (var i in validUsername) {
+         var valid = SignInService.isValidUsername(validUsername[i]);
+         expect(valid).toBeTruthy();
+       }
+       for (var i in invalidUsername) {
+         var valid = SignInService.isValidUsername(invalidUsername[i]);
+         expect(valid).toBeFalsy();
+       }
+     }])
+  );
 
 });
