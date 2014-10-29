6aecf63f2392db9e24101568d758478d378063e0
YiniYin <yiniski@gmail.com>
2014-10-30 00:08:34 +1300

    adds constants module to angular module

--- carbone-iron/app/scripts/app.js
+++ carbone-iron/app/scripts/app.js
@@ -1,18 +1,17 @@
-// Ionic Starter App
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
+
 'use strict';
-angular.module('CarboneIron', ['ionic', 'config', 'CarboneIron.services', 'CarboneIron.controllers'])
+
+angular.module('CarboneIron', ['ionic', 'config', 'CarboneIron.services',
+'CarboneIron.controllers', 'CarboneIron.constants'])
 
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+    // Hide the accessory bar by default (remove this to show the accessory
+    // bar above the keyboard for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -23,12 +22,9 @@ angular.module('CarboneIron', ['ionic', 'config', 'CarboneIron.services', 'Carbo
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+.config(function($stateProvider, $urlRouterProvider, USER_ROLES) {
+
   $stateProvider
     .state('signin', {
       url: '/sign-in',
