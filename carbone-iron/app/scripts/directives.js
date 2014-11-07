5b89d09bc308baaeb673f90a47f289f58bd57789
YiniYin <yiniski@gmail.com>
2014-11-08 12:21:16 +1300

    adds formAutofillFix directiveS

--- carbone-iron/app/scripts/directives.js
+++ carbone-iron/app/scripts/directives.js
@@ -3,6 +3,28 @@
 angular.module('CarboneIron.directives', [])
 
 
+.directive('formAutofillFix', function ($timeout) {
+  return function (scope, element, attrs) {
+    element.prop('method', 'post');
+    if (attrs.ngSubmit) {
+      $timeout(function () {
+        element
+          .unbind('submit')
+          .bind('submit', function (event) {
+            event.preventDefault();
+            element
+              .find('input, textarea, select')
+              .trigger('input')
+              .trigger('change')
+              .trigger('keydown');
+            scope.$apply(attrs.ngSubmit);
+          });
+      });
+    }
+  };
+});
+
+
 .directive('signInDialog', function (AUTH_EVENTS) {
   return {
     restrict: 'A',
@@ -17,4 +39,5 @@ angular.module('CarboneIron.directives', [])
       scope.$on(AUTH_EVENTS.sessionTimeout, showDialog);
     }
   };
+  
 });
