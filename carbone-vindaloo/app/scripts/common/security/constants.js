df9e95a0822903e3d988dc0c15cad2a081074a8c
YiniYin <yiniski@gmail.com>
2014-12-05 00:29:39 +1300

    adds user role masks

--- carbone-vindaloo/app/scripts/common/security/constants.js
+++ carbone-vindaloo/app/scripts/common/security/constants.js
@@ -1,20 +1,39 @@
 'use strict';
 
-angular.module('common.security.constants', [])
+var securityConstants = angular.module('common.security.constants', [])
 
-.constant('AUTH_EVENTS', {
-  signinSuccess: 'auth-login-success',
-  signinFailed: 'auth-login-failed',
-  signoutSuccess: 'auth-logout-success',
-  sessionTimeout: 'auth-session-timeout',
-  notAuthenticated: 'auth-not-authenticated',
-  notAuthorized: 'auth-not-authorized'
-})
-
-.constant('USER_ROLES', {
-  all: '*',
-  admin: 'admin',
-  editor: 'editor',
-  member: 'member',
-  guest: 'guest'
+securityConstants.constant('AUTH_EVENTS', {
+    signinSuccess: 'auth-login-success',
+    signinFailed: 'auth-login-failed',
+    signoutSuccess: 'auth-logout-success',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
 });
+
+securityConstants.constant('USER_ROLES', {
+    all: '*',
+    admin: 'admin',
+    editor: 'editor',
+    member: 'member',
+    guest: 'guest'
+});
+
+securityConstants.constant('USER_ROLE_MASKS', {
+    all: 1,
+    member: 2,
+    friend: 4,
+    admin: 8
+});
+
+securityConstants.accessLevels = {
+    ALL:     USER_ROLE_MASKS.all |
+             USER_ROLE_MASKS.member |
+             USER_ROLE_MASKS.friend |
+             USER_ROLE_MASKS.admin,
+    MEMBER:  USER_ROLE_MASKS.member |
+             USER_ROLE_MASKS.admin, 
+    FRIEND:  USER_ROLE_MASKS.friend |
+             USER_ROLE_MASKS.admin,
+    ADMIN:   USER_ROLE_MASKS.admin       
+};
