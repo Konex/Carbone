7e60bfe6a6c5563db10364d00a8f21ec60c9d92a
YiniYin <yiniski@gmail.com>
2014-12-13 19:02:04 +1300

    adds timeout to check unique username

--- carbone-vindaloo/app/scripts/features/me/signin/directives.js
+++ carbone-vindaloo/app/scripts/features/me/signin/directives.js
@@ -50,22 +50,29 @@ signinDirectives.directive('signInDialog', [
 
 // TODO: wire it up for sign up feature
 signinDirectives.directive('ensureUnique', [
-    '$http',
+    '$http', 
+    '$timeout', 
 
-    function($http) {
+    function($http, $timeout) {
+        var checking = null;
         return {
             require: 'ngModel',
             link: function(scope, ele, attrs, c) {
-              scope.$watch(attrs.ngModel, function() {
-                $http({
-                    method: 'POST',
-                    url: '/api/createProfile/' + attrs.ensureUnique,
-                    data: {'field': attrs.ensureUnique}
-                }).success(function(data, status, headers, cfg) {
-                    c.$setValidity('unique', data.isUnique);
-                }).error(function(data, status, headers, cfg) {
-                    c.$setValidity('unique', false);
-                });
+              scope.$watch(attrs.ngModel, function(newVal) {
+                if (!checking) {
+                  checking = $timeout(function() {
+                    $http({
+                        method: 'POST',
+                        url: '/api/signup/checkIfUsernameUnique' + attrs.ensureUnique,
+                        data: {'field': attrs.ensureUnique}
+                    }).success(function(data, status, headers, cfg) {
+                        c.$setValidity('unique', data.isUnique);
+                        checking = null;
+                    }).error(function(data, status, headers, cfg) {
+                        checking = null;
+                    });
+                  }, 500);
+                }
               });
             }
         }
