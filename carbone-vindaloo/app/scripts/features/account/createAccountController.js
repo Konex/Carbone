7a551c66a8f2d27d21adcacd93045f36dbc88a20
YiniYin <yiniski@gmail.com>
2015-06-26 23:37:58 +1200

    create account html

--- carbone-vindaloo/app/scripts/features/account/createAccountController.js
+++ carbone-vindaloo/app/scripts/features/account/createAccountController.js
@@ -1,7 +1,8 @@
 'use strict';
-
 var createAccountController = angular.module('createAccount.controller', []);
 
 createAccountController.controller('CreateAccountCtrl', function($scope) {
-  
+	$scope.avatarClick = function () {
+		angular.element(document.querySelector('#avatarSelector'))[0].click();;
+	};  
 });
